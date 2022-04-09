c7be9afdfbb76e40f10272a4e3bbb98f4ba2226a
shivamp1998 <shivampandey98@gmail.com>
2022-04-09 16:27:06 +0530

    pushNotifications

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/compat/messaging';
 import 'firebase/compat/database'
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyC8hP1L-E0nWZH8WMRhQqH5rV02jmDLYNw",
-    authDomain: "push-notifications-20f90.firebaseapp.com",
-    projectId: "push-notifications-20f90",
-    storageBucket: "push-notifications-20f90.appspot.com",
-    messagingSenderId: "582786857700",
-    appId: "1:582786857700:web:4979122d7f9eb692b0caf1"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC8hP1L-E0nWZH8WMRhQqH5rV02jmDLYNw",
+//     authDomain: "push-notifications-20f90.firebaseapp.com",
+//     projectId: "push-notifications-20f90",
+//     storageBucket: "push-notifications-20f90.appspot.com",
+//     messagingSenderId: "582786857700",
+//     appId: "1:582786857700:web:4979122d7f9eb692b0caf1"
+// };
 
 
 
